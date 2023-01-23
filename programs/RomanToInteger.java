662889c1e2f2e45c1cf4c5c2513ae24fdd31d1ec
arun <aathulur@asu.edu>
2023-01-22 23:58:32 -0500

    roman to number

--- programs/RomanToInteger.java
+++ programs/RomanToInteger.java
@@ -3,33 +3,84 @@ package programs;
 import java.util.HashMap;
 
 public class RomanToInteger {
+    static int ttl = 0;
+
+    static HashMap<Character, Integer> romanValues = new HashMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
     public static void main(String[] args) {
-        System.out.println(getInteger("MCMXCIV"));
+      //  System.out.println(" MCMXCIV :"+ romanToInt("MCMXCIV"));
+       // System.out.println("III :"+ romanToInt("II"));
+      //  System.out.println("LVIII :"+romanToInt("LVIII"));
+        long start = System.currentTimeMillis();
+        System.out.println("MCMXCVI :"+romanToInt("MCMXCVI"));
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        System.out.println("time : " +timeElapsed);
 
 
     }
+    static int checkNextChar(Character ch, Character nextChar) {
 
-    static int getInteger(String roman){
-        int number= 0;
-        HashMap<String,Integer> map = getMap();
+        switch (ch) {
+            case 'I' :{
+                if (nextChar.equals('V')) {
+                    return 4;
+                } else if (nextChar.equals('X')) {
+                    return 9;
+                }
+            }
+
+            case 'X' :{
+                if (nextChar.equals('L')) {
+                    return 40;
+                } else if (nextChar.equals('C')) {
+                    return 90;
+                }
+            }
+
+            case 'C' :{
+                if (nextChar.equals('D')) {
+                    return 400;
+                } else if (nextChar.equals('M')) {
+                    return 900;
+                }
+            }
+        }
+        return 0;
+    }
+
+    static int romanToInt(String roman){
+        int total= 0;
+        int ttlFromNextChar = 0;
+
         for(int i = 0; i<roman.length();i++){
             String ch = String.valueOf(roman.charAt(i));
-            number = number + map.get(ch);
+           // System.out.println(roman.charAt(i));
+            if(i<roman.length()-1){
+                if(ch.equals("I") || ch.equals("X") || ch.equals("C") &&!(i==roman.length())){
+                    ttlFromNextChar=checkNextChar(roman.charAt(i),roman.charAt(i+1));
+                    if(ttlFromNextChar>0){
+                        i++;
+                    }
+                }
+            }
+
+          if(ttlFromNextChar>0){
+              total = total + ttlFromNextChar ;
+              ttlFromNextChar = 0;
+
+          }else{
+              total = total + romanValues.get(ch);
+          }
         }
-        return number;
+        return total;
     }
-
-    static   HashMap<String,Integer> getMap(){
-        HashMap<String,Integer> romanMap = new HashMap<String, Integer>();
-        romanMap.put("I",1);
-        romanMap.put("V",5);
-        romanMap.put("X",10);
-        romanMap.put("L",50);
-        romanMap.put("C",100);
-        romanMap.put("D",500);
-        romanMap.put("M",1000);
-        return romanMap;
-    }
-
 
 }
