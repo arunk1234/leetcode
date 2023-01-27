4b32268f98b743c3c58468add07d2cc90d07dcdc
arun <athuluruak@gmail.com>
2023-01-27 00:52:19 -0500

    not fully working least common prefix

--- programs/LongestCommonPrefix.java
+++ programs/LongestCommonPrefix.java
@@ -1,18 +1,35 @@
 package programs;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-        String[] strs = {"flower", "flow", "flight"};
-        longestCommonPrefix(strs);
+        String[] strs = {"flower", "flow", "floght"};
+        System.out.println(longestCommonPrefix(strs));
 
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        //abcd, abcde , abcf - abc
-        for (int i = 0; i < strs.length; i++) {
-            System.out.println(strs[i]);
+        int index = 0;
+        int min = 0;
+        char[] arr = new char[4];
+        for(int i =0; i<strs.length;i++){
+            if(i ==1){
+                min = strs[i].length();
+            } else if (strs[i].length()<min) {
+                min = strs[i].length();
+            }
         }
-        return "";
+
+        for (int i = 0; i < min; i++) {
+            if(strs[index].charAt(i)==strs[index+i].charAt(i) && strs[index+1].charAt(i)==strs[2].charAt(i)){
+                arr[i]=strs[index].charAt(i);
+            }else{
+                break;
+            }
+        }
+        return Arrays.toString(arr);
 
     }
 }
